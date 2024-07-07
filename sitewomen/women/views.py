c8fcab1474a0c83cf7ef343aff81e21f07c0d782
YuryRass <superuser@mail.ru>
2024-07-07 18:42:56 +0300

    Маршрутизация и функции представления

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse, HttpRequest
 
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse('Главная страница приложения women.')
+
+
+def categories(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Категории статей</h1>")
