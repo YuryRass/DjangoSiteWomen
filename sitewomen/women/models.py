ea180be80c89f83bfbc8b1c2a3ad6af7b9f5ac3d
YuryRass <superuser@mail.ru>
2024-07-15 21:52:55 +0300

    used: class Meta

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 
@@ -7,3 +8,19 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return (f"Women(id={self.pk}, title='{self.title}', content={self.content[:30]}, "
+                f"time_create={datetime.strftime(self.time_create, '%d-%b-%Y-%H:%M:%S')}, "
+                f"time_update={datetime.strftime(self.time_update, '%d-%b-%Y-%H:%M:%S')}, "
+                f"is_published={self.is_published})"
+                )
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    class Meta:
+        ordering = ['-time_create']
+        indexes = [
+            models.Index(fields=['-time_create']),
+        ]
