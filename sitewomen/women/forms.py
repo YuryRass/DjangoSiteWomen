aa9e1eab410c483433c96484e61e319c73b1ca3e
Yurii <yurchik1997yurchik@gmail.com>
2024-10-03 22:46:38 +0300

    Отображение полей формы. Сохранение переданных данных в БД

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -3,9 +3,16 @@ from .models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+    title = forms.CharField(max_length=255, label="Заголовок", widget=forms.TextInput(attrs={'class': 'form-input'}))
+    slug = forms.SlugField(max_length=255, label="URL")
+    content = forms.CharField(widget=forms.Textarea(attrs={"cols": 70, "rows": 20}), required=False, label="Контент")
+    is_published = forms.BooleanField(required=False, label="Статус", initial=True)
+    cat = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        label="Категории",
+        empty_label="Категория не выбрана",
+    )
+    husband = forms.ModelChoiceField(
+        queryset=Husband.objects.all(), required=False, label="Муж",
+        empty_label="Не замужем",
+    )
