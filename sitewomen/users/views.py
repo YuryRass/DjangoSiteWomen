35adb9d1b3a23bd831656e62b27f1337603b1693
Yurii <yurchik1997yurchik@gmail.com>
2024-10-12 23:11:59 +0300

    Класс UserCreationForm

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.views.generic import CreateView
 
+from django.urls import reverse_lazy
 from users.forms import LoginUserForm, RegisterUserForm
 
 
@@ -9,15 +10,8 @@ class LoginUser(LoginView):
     template_name = 'users/login.html'
     extra_context = {'title': "Авторизация"}
 
-
-def register(request):
-    if request.method == "POST":
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return render(request, 'users/register_done.html')
-    else:
-        form = RegisterUserForm()
-    return render(request, 'users/register.html', {'form': form})
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': "Регистрация"}
+    success_url = reverse_lazy('users:login')
