3251b5cb9f860c7bb06579355b7a02b2b3af2f61
Yurii <yurchik1997yurchik@gmail.com>
2024-10-13 23:35:07 +0300

    Авторизация через e-mail. Профиль пользователя

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView
-from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import AbstractBaseUser, AnonymousUser
+from django.views.generic import CreateView, UpdateView
 
 from django.urls import reverse_lazy
-from users.forms import LoginUserForm, RegisterUserForm
+from typing import Any
+from users.forms import LoginUserForm, ProfileUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -15,3 +19,15 @@ class RegisterUser(CreateView):
     template_name = 'users/register.html'
     extra_context = {'title': "Регистрация"}
     success_url = reverse_lazy('users:login')
+
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+    extra_context = {'title': "Профиль пользователя"}
+
+    def get_success_url(self) -> Any:
+        return reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None) -> AbstractBaseUser | AnonymousUser:
+        return self.request.user
